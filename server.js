8f352ab9cd0a980da615b1801a2338d5ba7f5ede
churchillciobiz <churchillciobiz@gmail.com>
2020-01-09 03:07:04 +0300

    8th commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const dbase = mysql.createConnection({
   host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
   user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
   password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
-  port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+  port     : process.env.OPENSHIFT_MYSQL_DB_PORT || 8080,
   database : "mobiletdb"
 });
 
