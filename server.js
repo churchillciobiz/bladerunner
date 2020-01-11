1630bdf08fdf9abcbff22b4861e7b27242fae0b2
churchillciobiz <churchillciobiz@gmail.com>
2020-01-12 02:04:55 +0300

    14th commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(morgan('combined'))
 /* mysql connection */
 //Create db connection
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
 	host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
   user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
   password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
