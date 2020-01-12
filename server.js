09e7fbbbd0de45ce9a76909c1e7ce15619d237ce
churchillciobiz <churchillciobiz@gmail.com>
2020-01-12 10:14:11 +0300

    17th commit

--- server.js
+++ server.js
@@ -133,6 +133,7 @@ app.get('/getthis', function(req, res) {
   res.json({"message":"got this response"});
 })
 app.post('/postthis', function(req, res) {
+  console.log(request.body.telephone, request.body.passcode);
   res.json({"message":"got this response"});
 })
 app.post('/loginmobiletapp', (request, response) => {
